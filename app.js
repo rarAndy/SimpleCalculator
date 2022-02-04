31a18b6117ffe568111fa4889f3119e07e78752f
Andy <andrewryan128@gmail.com>
2022-02-03 19:54:32 -0500

    Added more functionality and CSS

--- app.js
+++ app.js
@@ -1,146 +1,130 @@
-const numberButtons = document.querySelectorAll(".numbers button")
-const output = document.querySelector(".output")
-
+const numberButtons = document.querySelectorAll(".numbers button");
+const calcWindow = document.querySelector(".calc-window");
+const line1 = document.querySelector("#line1");
+const inputline = document.querySelector(".input-line")
+const outputline = document.querySelector(".output-line")
 const buttons = document.querySelectorAll("button");
+
+const line = document.querySelectorAll(".line");
+let ans = 0
 buttons.forEach(button => {button.addEventListener('click', () => {
-    console.log(button.textContent)
-    addToOutput(button.textContent)
-})
-    
+    console.log(button.textContent);
+    addToOutput(button.textContent);
+    }); 
 });
 
 function addToOutput(input) {
     if (input == "=") {
-        //console.log(output.textContent);
-        evaluateOutput(output.textContent);
-    }
-    else {output.textContent += `${input} `};
+        calc(inputline.textContent);
+    }   else if (input == "Clear") {
+            inputline.textContent = '';
+    }   else if (input == "Clear All") {
+            inputline.textContent = '';
+            ans = 0;
+            for (let i = 5; i > 0; i--) {
+                line[i].children[0].textContent = ""
+                line[i].children[1].textContent = ""}
+    }   else if (input == "ans") {
+            inputline.textContent += 'ans';
+    }   else if ((input == "+" || input == "-" || input == "x" || input == "/" || input == "^") && (line[0].children[0].firstChild == null)) {
+            inputline.textContent += `ans ${input} `;
+    }   else if ((input == "+" || input == "-" || input == "x" || input == "/" || input == "^")) {
+            inputline.textContent += ` ${input} `;
+    }   else if (input == "(-)") {
+            inputline.textContent += `-`;
+    }   else {inputline.textContent += `${input}`};
 }
 
-function multiply (a, b) {
-    return a * b
-}
-
-function findWholeNumbers (input) {
+function parseNumsAndOps(input){
+    input = input.replace("ans", ans)
     input = input.replace(/\s/g, "");
-    if (input.includes("+") == true) {
-        /* let temp = input.slice(input.indexOf("+")-1,input.indexOf("+")+2);
-        let newNum = parseFloat(temp[0]) + parseFloat(temp[2]);
-        x = evaluateOutput(input.replace(temp, newNum));
-        return x */
-        num1=[]
-        num2=[]
-        operator = input.indexOf("+")
-        console.log(operator)
-        i = 1
-        console.log(input.charAt(operator-i))
-        console.log(parseInt(input.charAt(operator-i)))
-        while (parseInt(input.charAt(operator-i)) != "NaN") { 
-            if (input.indexOf(parseInt(input.charAt(operator-i))) == 0) {return}     
-            num1 += input.charAt(operator-i)
-            console.log(num1)
-            i += 1            
-        }
+    console.log(input);
+    let regex = /(-?\d*\.?\d+)|./g;
+    newArr = input.match(regex);
+    for (let i = 0; i < newArr.length-1 ; i++) {
+        if ((parseFloat(newArr[i]) && parseFloat(newArr[i+1])) ||
+            newArr[i] == ")" && parseFloat(newArr[i+1])) {
+            newArr.splice(i+1, 0, "+");
+            newArr.join();
+        };
+    }
+    return newArr;
+}
+
+function parSearch (array) {
+    par1 = 0;
+    par2 = 0;
+    for (let i = 0; i < array.length ; i++) {
         
+        if (array[i] == "(") {
+            par1 = i;
+        };
+        if (array[i] == ")") {
+            par2 = i;
+            break;
+        };
     }
-        
-    
+    parIndex = [par1, par2];
+    parExp = array.slice(par1, par2+1);
+    return {parExp, parIndex};
 }
 
-function evaluateOutput(input) {
-    input = input.replace(/\s/g, ""); // removes whitespace
-    
-    //locates first parenthesis, returns value without parenthesis and loops
-    if (input.includes("(") == true) {
-        let temp = input.slice(input.indexOf("(")+1,input.indexOf(")"));
-        let temp1 = input.slice(input.indexOf("("),input.indexOf(")")+1);
-        let tempOut = evaluateOutput(temp)
-        evaluateOutput(input.replace(temp1, tempOut));
-        return
-        }
-    if (input.includes("x") == true) {
-        nums = returnNums(input)
-        let newExp = `${nums[1]}x${nums[2]}`
-        //let temp = input.slice(input.indexOf("x")-1,input.indexOf("x")+2);
-        let newNum = nums[1] * nums[2];
-        x = evaluateOutput(input.replace(newExp, newNum));
-        return x
-        }
-    if (input.includes("/") == true) {
-        let temp = input.slice(input.indexOf("/")-1,input.indexOf("/")+2);
-        console.log(temp);
-        console.log(temp[0] / temp[2]);
-        let newNum = parseFloat(temp[0] / temp[2]);
-        Math.round(newNum * 1000000) / 1000000
-        console.log(newNum)
-        //console.log(input.replace(temp, newNum))
-        evaluateOutput(input.replace(temp, newNum));
-        return
-        }
-    if (input.includes("+") == true) {
-        let operator = input.indexOf("+");
-        i = 1
-        num1=[]
-        num2=[]
-        console.log(input.charAt(operator-i))
-        console.log(parseInt(input.charAt(operator-i)))
-        while (parseInt(input.charAt(operator-i)) != "NaN") { 
-                 
-            num1 += input.charAt(operator-i)
-            console.log(num1)
-            i += 1      
-            //if (input.indexOf(parseInt(input.charAt(operator-i))) == 0) {return}      
-        }
-        let newNum = parseFloat(temp[0]) + parseFloat(temp[2]);
-        x = evaluateOutput(input.replace(temp, newNum));
-        return x
-        }
-    if (input.includes("-") == true) {
-        let temp = input.slice(input.indexOf("-")-1,input.indexOf("-")+2);
-        let newNum = parseFloat(temp[0]) - parseFloat(temp[2]);
-        x = evaluateOutput(input.replace(temp, newNum));
-        return x
-        }
-    
-    else {
-        output.textContent = input;
-        console.log("returning", input)
-        return input
+function operations(array) {
+    console.log(array);
+    let math = {
+        "+": function(x, y) {return parseFloat(x) + parseFloat(y)},
+        "-": function(x, y) {return x - y},
+        "x": function(x, y) {return x * y},
+        "/": function(x, y) {return x / y},
+        "^": function(x, y) {return Math.pow(x, y)}
+    };
+    if (array.includes("^")) {
+        num = math["^"](array[array.indexOf("^")-1], array[array.indexOf("^")+1]);
+        array.splice(array.indexOf("^")-1, 3, num);
+        operations(array);
     }
+    if (array.includes("/")) {
+        num = math["/"](array[array.indexOf("/")-1], array[array.indexOf("/")+1]);
+        array.splice(array.indexOf("/")-1, 3, num);
+        operations(array);
+    };
+    if (array.includes("x")) {
+        num = math["x"](array[array.indexOf("x")-1], array[array.indexOf("x")+1]);
+        array.splice(array.indexOf("x")-1, 3, num);
+        operations(array);
+    };
+    if (array.includes("+")) {
+        num = math["+"](array[array.indexOf("+")-1], array[array.indexOf("+")+1]);
+        array.splice(array.indexOf("+")-1, 3, num);
+        operations(array);
+    };
+    return array;
 }
 
+function calc(input) {
+    expr = parseNumsAndOps(input);
 
+    while (expr.includes("(")) {
+        parExp = parSearch(expr).parExp;
+        parIndex = parSearch(expr).parIndex;
+        parInitExp = parExp.length;
+        parEval = operations(parExp);
+        expr.splice(parIndex[0], parInitExp, parEval[1]);
+    } 
 
-function returnNums(input) {
-    let operator = input.indexOf("x");
-    let str1 = input.slice(0,operator)
-    let str2 = input.slice(operator+1)
-    console.log(str2)
-    str1 = str1.split("").reverse().join("")
-    console.log(str1)
-
-    num1=""
-    num2=""
-    operators = ["+","-","/","x"]
-    for (let i = 0; i < str1.length; i++) {
-        console.log(typeof(parseInt(str1.charAt(i))))
-        if (isNaN(parseInt(str1.charAt(i))) == false) {
-            num1 += str1.charAt(i)
-        } else {
-            break
-        }; 
-    }
-
-    num1 = num1.split("").reverse().join("")
-
-    for (let i = 0; i < str2.length; i++) {
-        console.log(typeof(parseInt(str2.charAt(i))))
-        if (isNaN(parseInt(str2.charAt(i))) == false) {
-            num2 += str2.charAt(i)
-        } else {
-            break
-        }; 
-    }
-
-    return {num1, num2}
+    operations(expr);
+    outputline.textContent = expr;
+    ans = expr;
+    changeTextContent();
+    return ans;
 }
+
+function changeTextContent() {
+    for (let i = 5; i > 0; i--) {
+        line[i].children[0].textContent = line[i-1].children[0].textContent
+        line[i].children[1].textContent = line[i-1].children[1].textContent
+    }
+    inputline.textContent = '';
+    outputline.textContent = '';
+}
+
